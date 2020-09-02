e8632655e1359ea2bba9e1ec00a518109ef532d8
Asuka Takemura <assan.takemura@gamil.com>
2020-09-02 22:43:31 +0900

    Count完成

--- L4Scount/ViewController.swift
+++ L4Scount/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+        
+    }
+    
+    @IBAction func min(){
+        number = number - 1
+        label.text = String(number)
+        
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+        
+    }
+    
+    @IBAction func kake(){
+        number = number * 2
+        label.text = String(number)
+        
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+        
+    }
+    
+    @IBAction func waru(){
+        number = number / 2
+        label.text = String(number)
+        
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+        
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        label.textColor = UIColor.black
+    }
 
 }
 
